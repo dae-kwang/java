0adf730e3bc40361b5f5efff80fb4331e7cb101c
dae-kwang <eorhkd0102@naver.com>
2023-12-08 17:15:33 +0900

    자바 1일차

--- java_jdk/src/day01/HelloWorld.java
+++ java_jdk/src/day01/HelloWorld.java
@@ -3,7 +3,7 @@ package day01;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		//sysout->ctrl + space는 자동으로 , 콘솔에 출력할 때 사용
 		System.out.println("Hello World!");
 		
 	}
