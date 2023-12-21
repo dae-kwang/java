25755a4e5d6080cd61deb8df8ed25d2af6e91cf6
dae-kwang <eorhkd0102@naver.com>
2023-12-21 15:47:47 +0900

    자바 10일차 BoardMain 수정

---
+++ java_jdk/src/day10/board/BoardMain.java
@@ -0,0 +1,19 @@
+package day10.board;
+
+public class BoardMain {
+
+	public static void main(String[] args) {
+		/* 게시판에서 게시글 관리를 위한 콘솔 프로그램 작성하세요.
+		 * - 제한사항 정리
+		 * 	
+		 * 1. 필요한 기능을 정리해서 메뉴로 출력
+		 * 	게시물 작성 및 편집
+		 * 댓글 기능(비밀글)
+		 * 좋아요 기능
+		 * 신고 및 차단
+		 * 2. 기능을 구현
+		 * */
+
+	}
+
+}
